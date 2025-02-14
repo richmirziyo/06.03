5357c794b79c67792b16d4775ef90939b0f6efc3
mirziyo <mirziyo>
2025-02-14 16:00:32 +0500

    Update footer links and improve code formatting

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
 // style
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-container">
         <p>All rights reserved.</p>
-        <a href="https://www.youtube.com/@akhrorweb">Akhror Soliev</a>
+        <a href="https://t.me/Kxan_Muz">Rich Mirziyobek</a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
